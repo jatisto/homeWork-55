1d96b7e1eeae79d0467109a58be79bf9e5c2eaef
EugeneTM <j.atisto@gmail.com>
2018-10-10 20:12:11 +0600

    add mig

--- HomeWork-55/HomeWork-55/Data/Migrations/20181010135528_add name.Designer.cs
+++ HomeWork-55/HomeWork-55/Data/Migrations/20181010135528_add name.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Homework_55.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181010135528_add name")]
+    partial class addname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- HomeWork-55/HomeWork-55/Models/ApplicationUser.cs
+++ HomeWork-55/HomeWork-55/Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace Homework_55.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string _name { get; set; }
     }
 }
