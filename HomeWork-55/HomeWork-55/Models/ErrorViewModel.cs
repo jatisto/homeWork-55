f81409e67c51b0647da0e645e2593480b285797e
Joni <j.atisto@gmail.com>
2018-10-10 10:59:42 +0600

    create auth

---
+++ HomeWork-55/HomeWork-55/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWork_55.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
